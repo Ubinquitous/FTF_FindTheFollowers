d8067ec01061d459c79281b8d3e90b3cf274b401
Ubinquitous <ubinquitous1@gmail.com>
2022-11-24 16:04:13 +0900

    FIX : 라우팅

--- src/App.tsx
+++ src/App.tsx
@@ -1,82 +1,18 @@
-import axios from 'axios';
-import { Octokit } from 'octokit';
-import React, { useEffect, useState } from 'react';
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import NotFound from './pages/NotFound';
+import Result from './pages/Result';
 
 const App = () => {
-  const [followerArray, setFollowerArray]: any = useState([]);
-  const [followingArray, setFollowingArray]: any = useState([]);
-  const [loading, setLoading] = useState(false);
-
-  useEffect(() => {
-    const octokit = new Octokit({
-      auth: process.env.REACT_APP_GITHUB_KEY,
-    })
-    for (let i = 1; i <= 1000; i++) {
-      const follower = octokit.request('GET /users/{username}/followers{?per_page,page}', {
-        username: 'Ubinquitous',
-        per_page: 100,
-        page: i
-      })
-      follower.then((res) => {
-        if (res.data.length === 0) {
-          return;
-        } else {
-          for (let j = 1; j <= res.data.length; j++) {
-            if (res.data[j]?.login !== undefined) {
-              followerArray.push(res.data[j]?.login)
-            }
-          }
-        }
-      })
-
-      const following = octokit.request('GET /users/{username}/following{?per_page,page}', {
-        username: 'Ubinquitous',
-        per_page: 100,
-        page: i
-      })
-
-      following.then((res) => {
-        if (res.data.length === 0) {
-          setLoading(true)
-          return;
-        } else {
-          for (let j = 1; j <= res.data.length; j++) {
-            if (res.data[j]?.login !== undefined) {
-              followingArray.push(res.data[j]?.login)
-            }
-          }
-        }
-      })
-    }
-
-  }, []);
-
   return (
-    <div>
-      <button onClick={() => { console.log(followerArray, followingArray, followingArray.filter((x: any) => !followerArray.includes(x)), followerArray.filter((x: any) => !followingArray.includes(x))) }}>CLICK</button>
-      <div>
-        <h1>Follower List</h1>
-        {loading ?
-          <>
-            {followerArray?.map((data: any, index: number) => (
-              <div key={data?.id}>
-                <span>{data?.login}</span>
-                <br />
-              </div>
-            ))}
-          </> : 'loading...'}
-        <h1>Following List</h1>
-        {loading ?
-          <>
-            {followingArray?.map((data: any, index: number) => (
-              <div key={data?.id}>
-                <span>{data?.login}</span>
-                <br />
-              </div>
-            ))}
-          </> : 'loading...'}
-      </div>
-    </div>
+    <Router>
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/search/:id' element={<Result />} />
+        <Route path='*' element={<NotFound />} />
+      </Routes>
+    </Router>
   );
 };
 
