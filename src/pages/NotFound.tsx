bb3d2645146ce59f505c0b8ca906b15553701d95
Ubinquitous <ubinquitous1@gmail.com>
2022-11-25 08:30:24 +0900

    ADD : 스타일링

--- src/pages/NotFound.tsx
+++ src/pages/NotFound.tsx
@@ -1,11 +1,16 @@
 import React from 'react';
 import Header from '../components/Header';
+import '../style/Home.scss';
 
 const NotFound = () => {
     return (
         <div>
             <Header />
-            not found
+            <div className='notf-wrap'>
+                <span className='notfound'>
+                    Page is not found. Sorry.
+                </span>
+            </div>
         </div>
     );
 };
