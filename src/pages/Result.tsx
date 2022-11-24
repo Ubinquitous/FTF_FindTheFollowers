c02bafca8d8294c7425acbbac21addda5e2485a5
Ubinquitous <ubinquitous1@gmail.com>
2022-11-24 16:03:58 +0900

    ADD : 결과

--- src/pages/Result.tsx
+++ src/pages/Result.tsx
@@ -1,9 +1,11 @@
 import React from 'react';
+import Header from '../components/Header';
 
 const Result = () => {
     return (
         <div>
-
+            <Header />
+            result
         </div>
     );
 };
